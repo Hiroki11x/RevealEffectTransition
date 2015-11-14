424c80bffdf5bd2ba7b562ffb141a809c3f09d45
Hiroki11x <hiroki11x@gmail.com>
2015-11-14 14:57:17 +0900

    Legacy

---
+++ app/src/main/java/transition/ripple/hiroki11x/rippletransitionanimation/ActivityOptionsExtended.java
@@ -0,0 +1,33 @@
+//package transition.ripple.hiroki11x.rippletransitionanimation;
+//
+//import android.app.ActivityOptions;
+//
+///**
+// * Created by hirokinaganuma on 15/11/14.
+// */
+////public class ActivityOptionsExtended extends ActivityOptions {
+////
+////    int[] pts = new int[2];
+////
+////    int mStartX;
+////    int mStartY;
+////    int mWidth;
+////    int mHeight;
+////
+////
+////    public ActivityOptionsExtended(int[] pts, int mStartX, int mStartY, int mWidth, int mHeight) {
+////        this.pts = pts;
+////        this.mStartX = mStartX;
+////        this.mStartY = mStartY;
+////        this.mWidth = mWidth;
+////        this.mHeight = mHeight;
+////    }
+////
+////    ActivityOptionsExtended(int startX, int startY,int width,int height){
+////        this.mStartX = pts[0] + startX;
+////        this.mStartY = pts[1] + startY;
+////        this.mWidth = width;
+////        this.mHeight = height;
+////    }
+//
+//}
